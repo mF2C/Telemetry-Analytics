a661279966337c6474481ec9169d9980e572669d
aleckey <aleckey@aleckey-MOBL.ger.corp.intel.com>
2018-04-20 13:21:40 +0100

    Adding docker compose support
    Added a list of plugins and config support to build container
    
    Signed-off-by: aleckey

---
+++ Telemetry/download.sh
@@ -0,0 +1,28 @@
+# download snap framework
+wget http://snap.ci.snap-telemetry.io/snap/latest_build/linux/x86_64/snaptel -O snaptel
+chmod +x snaptel
+
+# download required plugins
+# collectors
+wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-psutil/latest/linux/x86_64/snap-plugin-collector-psutil -O snap-plugin-collector-psutil
+wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-meminfo/latest/linux/x86_64/snap-plugin-collector-meminfo -O snap-plugin-collector-meminfo
+wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-cpu/latest/linux/x86_64/snap-plugin-collector-cpu -O snap-plugin-collector-cpu
+wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-docker/latest/linux/x86_64/snap-plugin-collector-docker -O snap-plugin-collector-docker
+wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-disk/latest/linux/x86_64/snap-plugin-collector-disk -O snap-plugin-collector-disk 
+wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-interface/latest/linux/x86_64/snap-plugin-collector-interface -O snap-plugin-collector-interface
+wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-use/latest/linux/x86_64/snap-plugin-collector-use -O snap-plugin-collector-use
+# publishers
+wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-publisher-influxdb/latest/linux/x86_64/snap-plugin-publisher-influxdb -O snap-plugin-publisher-influxdb
+
+# load plugins 
+./snaptel plugin load snap-plugin-collector-psutil
+./snaptel plugin load snap-plugin-collector-cpu
+./snaptel plugin load snap-plugin-collector-meminfo
+./snaptel plugin load snap-plugin-collector-docker
+./snaptel plugin load snap-plugin-collector-disk
+./snaptel plugin load snap-plugin-collector-interface
+./snaptel plugin load snap-plugin-collector-use
+./snaptel plugin load snap-plugin-publisher-influxdb
+
+# configure the plugins
+./snaptel task create -t task.json
--- Telemetry/readme.md
+++ Telemetry/readme.md
@@ -1,5 +1,16 @@
+# Telemetry
+
 ## Introduction
 
-The telemetry framework will be used to capture performance metrics on the node the service is executing.
+mF2C will use the Snap telemetry framework to capture performance metrics on each node. The metrics are currently stored in a TSDB (Influx).
 
-Installation instructions will follow...
+# Installation instructions
+
+The Docker compose script will create a container with 2 services
+-	Snap framework: which will run a list of telemetry probes
+-	InfluxDB: a Time Series database
+
+```
+$ docker-compose up 
+$ ./download.sh
+```
