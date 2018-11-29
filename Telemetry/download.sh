84ef524159151a7068356ad58b65b53fb9f28cb5
aleckey <alec.leckey@gmail.com>
2018-11-29 14:43:44 +0000

    Features to the SnapTemp container
    - Updated snap container to ubuntu 16.04
    - Bundled the SNAP plugins, save downloading

--- Telemetry/download.sh
+++ Telemetry/download.sh
@@ -1,17 +1,17 @@
 # download snap framework
-wget http://snap.ci.snap-telemetry.io/snap/latest_build/linux/x86_64/snaptel -O snaptel
+# wget http://snap.ci.snap-telemetry.io/snap/latest_build/linux/x86_64/snaptel -O snaptel
 chmod +x snaptel
 
 # download required plugins
 # collectors
-wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-psutil/latest/linux/x86_64/snap-plugin-collector-psutil -O snap-plugin-collector-psutil
-wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-meminfo/latest/linux/x86_64/snap-plugin-collector-meminfo -O snap-plugin-collector-meminfo
-wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-cpu/latest/linux/x86_64/snap-plugin-collector-cpu -O snap-plugin-collector-cpu
-wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-docker/latest/linux/x86_64/snap-plugin-collector-docker -O snap-plugin-collector-docker
-wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-disk/latest/linux/x86_64/snap-plugin-collector-disk -O snap-plugin-collector-disk 
-wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-interface/latest/linux/x86_64/snap-plugin-collector-interface -O snap-plugin-collector-interface
+#wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-psutil/latest/linux/x86_64/snap-plugin-collector-psutil -O snap-plugin-collector-psutil
+#wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-meminfo/latest/linux/x86_64/snap-plugin-collector-meminfo -O snap-plugin-collector-meminfo
+#wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-cpu/latest/linux/x86_64/snap-plugin-collector-cpu -O snap-plugin-collector-cpu
+#wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-docker/latest/linux/x86_64/snap-plugin-collector-docker -O snap-plugin-collector-docker
+#wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-disk/latest/linux/x86_64/snap-plugin-collector-disk -O snap-plugin-collector-disk 
+#wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-interface/latest/linux/x86_64/snap-plugin-collector-interface -O snap-plugin-collector-interface
 # publishers
-wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-publisher-influxdb/latest/linux/x86_64/snap-plugin-publisher-influxdb -O snap-plugin-publisher-influxdb
+#wget http://snap.ci.snap-telemetry.io/plugins/snap-plugin-publisher-influxdb/latest/linux/x86_64/snap-plugin-publisher-influxdb -O snap-plugin-publisher-influxdb
 
 # load plugins 
 ./snaptel --url http://snap:8181 plugin load snap-plugin-collector-psutil
